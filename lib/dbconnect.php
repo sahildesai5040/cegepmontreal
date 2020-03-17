812b7df0399f024f6cbd92658a5cf6387490fab7
sahildesai5040 <62262579+sahildesai5040@users.noreply.github.com>
2020-03-17 16:35:15 -0300

    connect

--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -1,17 +1,11 @@
 <?php
-echo "file called";
+
 class dbconnect
 {
     function connect()
-    {/*
-       if(mysqli_connect("den1.mysql1.gear.host","cegepmontreal","Welcome22@","cegepmontreal"))
-       {
-	       echo "Connection Success";
-       }
-	else
-	{
-		echo "connection Not success";
-	}*/
+    {
+      $connection=mysqli_connect("den1.mysql1.gear.host","cegepmontreal","Welcome22@","cegepmontreal");
+      echo $connection;
 	    
 	    
 	
