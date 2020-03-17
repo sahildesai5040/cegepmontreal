867027eb80e7d1d0ee4f21134bbdce65b7ce2197
sahildesai5040 <62262579+sahildesai5040@users.noreply.github.com>
2020-03-17 16:46:52 -0300

    up db

--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -1,8 +1,12 @@
 <?php
 
-
-      $connection=mysqli_connect("den1.mysql1.gear.host","cegepmontreal","Welcome22@","cegepmontreal");
-	    return $connection;
-      	
+class dbconnect
+{
+    function connect()
+    {
+        $connection=mysqli_connect("den1.mysql1.gear.host","cegepmontreal","Welcome22@","cegepmontreal");
+				return $connection;
+    }
+}
 
 ?>
